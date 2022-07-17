ec420a7a897baa451d930e397e1afbe1a5fbee43
fahim <mdfahim1194@gmail.com>
2022-07-17 12:05:01 +0600

    Auto mapper

---
+++ API/Helper/AutoMapperProfiles.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using API.Entities;
+using AutoMapper;
+
+namespace API.Helper
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Product, ProductToReturnDto>()
+                .ForMember(dest => dest.ProductBrand, opt => opt.MapFrom(src => src.ProductBrand.Name))
+                .ForMember(dest => dest.ProductType, opt => opt.MapFrom(src => src.ProductType.Name))
+                .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+        }
+    }
+}
