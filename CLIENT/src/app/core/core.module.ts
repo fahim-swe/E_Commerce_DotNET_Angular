013b183809f3881e354c18f8a4f061acaf743c08
fahim <mdfahim1194@gmail.com>
2022-07-22 01:14:24 +0600

    fix error

--- CLIENT/src/app/core/core.module.ts
+++ CLIENT/src/app/core/core.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule, Routes } from '@angular/router';
+import { SectionHeaderComponent } from './section-header/section-header.component';
+
 
 
 @NgModule({
-  declarations: [NavBarComponent],
+  declarations: [NavBarComponent, SectionHeaderComponent],
   imports: [
     CommonModule,
     RouterModule
   ], 
-  exports: [NavBarComponent]
+  exports: [NavBarComponent, SectionHeaderComponent]
 })
 export class CoreModule { }
