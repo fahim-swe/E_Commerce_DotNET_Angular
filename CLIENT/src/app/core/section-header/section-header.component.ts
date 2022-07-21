a7192f6d4d46be7685cbe93e07b1b70fb4a30698
fahim <mdfahim1194@gmail.com>
2022-07-22 01:13:50 +0600

    section header

---
+++ CLIENT/src/app/core/section-header/section-header.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-header',
+  templateUrl: './section-header.component.html',
+  styles: [
+  ]
+})
+export class SectionHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
